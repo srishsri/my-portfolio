8113400d69635ad95da949e99229f2f0665bb076
sridhar <sridhar14895@gmail.com>
2024-10-19 22:55:37 +0530

    navbar css updated

--- port/src/components/Hero/Hero.js
+++ port/src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
                 <div className="hero-mask opacity-8 bg-dark"></div>
                 <div className='hero-content'>
                     <h3>Welcome</h3>
-                    <h1>I'm <span>{text}</span><Cursor /> Developer.</h1>
+                    <h1>I'm <span>{text}</span><Cursor/> Developer.</h1>
                     <p>I am a Frontend developer from Coimbatore , with 5+ years of experience.</p>
                     <div className='hero-buttons'>
                         <a href="#" className='connect-me'> Contact With Me </a>
